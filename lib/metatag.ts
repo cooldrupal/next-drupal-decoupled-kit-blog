76d5612bd7726f46384fe1e647eff4e332e5001e
cooldrupal <cooldrupal@gmail.com>
2025-07-25 16:37:43 +0300

    Add getMetatag() function

---
+++ lib/metatag.ts
@@ -0,0 +1,15 @@
+
+export function getMetatag(entity: any) {
+  if (entity.metatag) {
+    const metaAttributes = entity.metatag
+      .filter((item: any) => item.tag === "meta" && item.attributes?.name && item.attributes?.content)
+      .reduce((acc: any, item: any) => {
+        acc[item.attributes.name] = item.attributes.content;
+        return acc;
+      }, {} as Record<string, string>);
+    return metaAttributes;
+  }
+  else {
+    return {};
+  }
+}
